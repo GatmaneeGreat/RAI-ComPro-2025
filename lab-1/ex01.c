a7614937d1380238772254c5112101c64500d833
GatmaneeGreat <gatmaneedum@gmail.com>
2025-07-18 10:01:56 +0000

    completelab1

---
+++ lab-1/ex01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello, World !\n");
+    printf("Wellcome to Robotics and AI !\n");
+    return 0;
+}
---
+++ lab-1/ex02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is  %s\n","Gatmanee Tasarin");
+    printf("I am  %d,%s\n",20,"year old");
+    printf("I am  %d,%s\n",163,"cm tall");
+    return 0;
+}
