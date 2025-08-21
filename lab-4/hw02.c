471901af45a926189bd880f18606489982f0ddbe
GatmaneeGreat <gatmaneedum@gmail.com>
2025-08-21 15:34:44 +0000

    com

---
+++ lab-4/hw02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int num,sum=0;
+
+    do
+    {
+        printf("Enter a number : ");
+        scanf("%d",&num);
+        sum += num;
+
+    }
+    while(num != 0);
+    {
+       
+        printf("result : %d\n",sum+=num);
+    }
+
+    return 0;
+}
+
